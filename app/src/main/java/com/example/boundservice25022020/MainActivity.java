e43692d54c13f563c3097a6826b48d829d494439
phatdroid94 <phatdroid94@gmail.com>
2020-05-09 21:20:36 +0700

    khoi tao bound service

--- app/src/main/java/com/example/boundservice25022020/MainActivity.java
+++ app/src/main/java/com/example/boundservice25022020/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.boundservice25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEdtSt1, mEdtSt2;
+    Button mBtnTinhtoan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtSt1 = findViewById(R.id.edittextSothu1);
+        mEdtSt2 = findViewById(R.id.edittextSothu2);
+        mBtnTinhtoan = findViewById(R.id.buttonXuly);
+
+        mBtnTinhtoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+            }
+        });
     }
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.MyBinder myBinder = (MyService.MyBinder) service;
+            Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Toast.makeText(MainActivity.this, "Disconnect", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
