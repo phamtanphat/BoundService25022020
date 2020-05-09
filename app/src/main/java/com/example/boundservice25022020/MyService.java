aa397fdc5cce3700ce37409415f21ec2d8eeec75
phatdroid94 <phatdroid94@gmail.com>
2020-05-09 21:31:57 +0700

    finish

--- app/src/main/java/com/example/boundservice25022020/MyService.java
+++ app/src/main/java/com/example/boundservice25022020/MyService.java
@@ -3,6 +3,7 @@ package com.example.boundservice25022020;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 public class MyService extends Service {
 
     MyBinder myBinder;
+    int ketqua = -1;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -21,12 +23,29 @@ public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        final Integer st1 = intent.getIntExtra("st1",-1);
+        final Integer st2 = intent.getIntExtra("st2",-1);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ketqua = st1 + st2;
+            }
+        },2000);
         return myBinder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Toast.makeText(this, "onRebind", Toast.LENGTH_SHORT).show();
     }
 
     class MyBinder extends Binder {
         public MyService getService(){
             return MyService.this;
         }
+    }
+    public int getKetqua(){
+        return ketqua;
     }
 }
